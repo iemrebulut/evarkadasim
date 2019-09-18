71494e188728571d92be8bb72da6f4d1283b52c2
İhsan Emre Bulut <emrebulut@emre-mac.local>
2019-09-18 09:40:52 -0700

    Initial Commit

---
+++ EvArkadasim/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  EvArkadasim
+//
+//  Created by İhsan Emre Bulut on 18.09.2019.
+//  Copyright © 2019 İhsan Emre BULUT. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ EvArkadasim/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  EvArkadasim
+//
+//  Created by İhsan Emre Bulut on 18.09.2019.
+//  Copyright © 2019 İhsan Emre BULUT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
