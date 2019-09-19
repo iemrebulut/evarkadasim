2774f5162b5516d8f3f6c6df3d7a3a032f59fe4b
İhsan Emre Bulut <emrebulut@Emre-Mac.local>
2019-09-19 03:53:21 -0700

    Login add, Register update

---
+++ EvArkadasim/Controller/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  EvArkadasim
+//
+//  Created by İhsan Emre Bulut on 19.09.2019.
+//  Copyright © 2019 İhsan Emre BULUT. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
