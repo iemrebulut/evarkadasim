eeb1ec1798e7f590ee53ecd025eb1434b931ab82
İhsan Emre Bulut <emrebulut@Emre-Mac.local>
2019-09-19 02:18:50 -0700

    first commit

---
+++ EvArkadasim/Controller/RegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterViewController.swift
+//  EvArkadasim
+//
+//  Created by İhsan Emre Bulut on 18.09.2019.
+//  Copyright © 2019 İhsan Emre BULUT. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ EvArkadasim/Extensions/UIViewController.swift
@@ -0,0 +1,11 @@
+//
+//  alertExtensions.swift
+//  EvArkadasim
+//
+//  Created by İhsan Emre Bulut on 18.09.2019.
+//  Copyright © 2019 İhsan Emre BULUT. All rights reserved.
+//
+
+import UIKit
+
+extension 
